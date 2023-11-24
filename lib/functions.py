bca1f68f5d61bc92592a0a1b2ded55c200dbc3e2
Brenda <brendakungu04@gmail.com>
2023-11-24 16:31:34 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()    
+    #pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Anita")
+    #pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()    
+    #pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    
+    print(add(45,55))
+
+    #pass
 
 def halve(number):
-    pass
+    return number/2
+
+    print(divide(30/2))
+    #pass
